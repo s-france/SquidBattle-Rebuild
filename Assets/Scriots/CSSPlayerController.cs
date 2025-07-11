a889e761d3d0173ebcb8abf9a8912157d3eec18d
s-france <sfrance1218@gmail.com>
2025-07-10 22:27:35 -0400

    CSS start
    BANG

---
+++ Assets/Scriots/CSSPlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CSSPlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnASelect(InputAction.CallbackContext ctx)
+    {
+
+
+    }
+
+    public void OnBCancel(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void OnLeftL(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void OnRightR(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void OnLeftTrig(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void OnRightTrig(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void OnHoldBack(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void OnJoinLR(InputAction.CallbackContext ctx)
+    {
+
+    }
+    
+    public void OnShoulderJoinRTLT(InputAction.CallbackContext ctx)
+    {
+        
+    }
+}
---
+++ Assets/Scriots/LevelControllers/CSSLevelController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSSLevelController : MenuLevelController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set all players input actionmaps to CharacterSelect
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
